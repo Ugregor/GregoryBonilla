869d339b62246b544ea3e9a9cfecc9910b107e14
bcepeda_98@yahoo.com <44bc3p3d4>
2022-04-30 10:35:18 -0500

    Agregar archivos de proyecto.

---
+++ GregoryBonilla/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GregoryBonilla
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSesion_Clicked(object sender, EventArgs e)
+        {
+            string usu = txtUsu.Text;
+            string pass = txtPass.Text;
+
+            if(usu == "GregoryBonilla" && pass == "uisrael2022")
+            {
+                await Navigation.PushAsync(new Registro(usu));
+            }
+            else if (string.IsNullOrEmpty(usu) && string.IsNullOrEmpty(pass))
+            {
+                await DisplayAlert("Alerta", "Complete los campos", "Cerrar");
+            }
+            else
+            {
+                await DisplayAlert("Alerta", "Usuario o Contraseña Incorrecto", "Cerrar");
+            }
+        }
+    }
+}
---
+++ GregoryBonilla/Registro.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GregoryBonilla
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usu)
+        {
+            InitializeComponent();
+            lblusu.Text = usu;
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            double total = 3000;
+            double cuota = Convert.ToDouble(txtCuota.Text);
+            if (cuota > total)
+            {
+                DisplayAlert("Aleta", "La cuota inicial es mayor que el valor del curso", "Cerrar");
+            }
+            else
+            {
+                double v1 = (total - cuota) / 5;
+                double vCuota = (v1 * 0.05)+ v1;
+                double vFinal = vCuota * 5;
+                txtPagos.Text = vCuota.ToString();
+                txtTotal.Text = vFinal.ToString();
+            }
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNom.Text;
+            string vCuota = txtPagos.Text;
+            string vFinal = txtTotal.Text;
+            await Navigation.PushAsync(new Resumen(nombre, vCuota, vFinal));
+        }
+
+    }
+}
